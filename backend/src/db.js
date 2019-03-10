d961c408c91d1b9a721dace17c2dae7be711790b
mhonert <48153876+mhonert@users.noreply.github.com>
2019-03-10 20:01:58 +0100

    Setup GraphQL Yoga Server; add GraphQL query and mutation for items

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+// Connect to the remote prisma DB and give us the ability to query it with JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
